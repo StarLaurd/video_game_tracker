14b7720b73c0d2df755b8de70b7c9b8ab1a41069
anonymous anonymous <laurakitterman@live.com>
2021-12-04 22:00:28 +0000

    Generate graphiti resources for publishers

---
+++ app/controllers/api/v1/publishers_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::PublishersController < Api::V1::GraphitiController
+  def index
+    publishers = PublisherResource.all(params)
+    respond_with(publishers)
+  end
+
+  def show
+    publisher = PublisherResource.find(params)
+    respond_with(publisher)
+  end
+
+  def create
+    publisher = PublisherResource.build(params)
+
+    if publisher.save
+      render jsonapi: publisher, status: 201
+    else
+      render jsonapi_errors: publisher
+    end
+  end
+
+  def update
+    publisher = PublisherResource.find(params)
+
+    if publisher.update_attributes
+      render jsonapi: publisher
+    else
+      render jsonapi_errors: publisher
+    end
+  end
+
+  def destroy
+    publisher = PublisherResource.find(params)
+
+    if publisher.destroy
+      render jsonapi: { meta: {} }, status: 200
+    else
+      render jsonapi_errors: publisher
+    end
+  end
+end
